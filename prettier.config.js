59c071749331d6d4eb9d933a151f565edab0d0f0
Nolb226 <hn26677@gmail.com>
2023-12-22 00:27:21 +0700

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  tailwindConfig: "./tailwind.config.js",
+  tailwindFunctions: ["clsx"],
+};
